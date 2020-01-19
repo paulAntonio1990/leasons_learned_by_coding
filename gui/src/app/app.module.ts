9b14e34dcfe84c90aec1187a69198d47f8369358
Paul <paulantonioifrmi@yahoo.com>
2020-01-19 16:21:17 +0200

    gui welcome page added

--- gui/src/app/app.module.ts
+++ gui/src/app/app.module.ts
@@ -2,13 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { UsersListComponent } from './users/users-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    UsersListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: 'users', component: UsersListComponent },
+      { path: 'homePage', component: HomePageComponent },
+      { path: '', redirectTo: 'homePage', pathMatch: 'full' },
+      { path: '**', redirectTo: 'homePage', pathMatch: 'full' }      
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
